49cf01fc31ce9717b624096197bfb80e2385198e
Daniel Viernes <ddvrns@gmail.com>
2022-01-31 00:02:13 +0800

    improve landing section texts

--- src/Components/LandingSection.tsx
+++ src/Components/LandingSection.tsx
@@ -8,7 +8,7 @@ const LandingSection = () => {
         <Container className='landing-section-container'>
             <Row>
                 <Col sm={12}><h1>Clean and delicious rice, right at your doorstep!</h1></Col>
-                <Col sm={12}><h4>Available in retail and wholesale.</h4></Col>
+                <Col sm={12}><h3>Available in retail and wholesale.</h3></Col>
                 <Col sm={12}><h4>Chat with us for availability and serviceable areas.</h4></Col>
             </Row>
         </Container>
