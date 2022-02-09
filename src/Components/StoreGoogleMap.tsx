b8b6ef0b6aa9ef037fea1fe6456986000e6da7e7
Daniel Viernes <ddvrns@gmail.com>
2022-02-10 03:07:58 +0800

    removed exposed key

--- src/Components/StoreGoogleMap.tsx
+++ src/Components/StoreGoogleMap.tsx
@@ -7,7 +7,7 @@ const hgLng = 120.9355877;
 const StoreGoogleMap = () => {
   return(
     <GoogleMaps
-      apiKey={"AIzaSyCx0IYUpeh-izbzfQ_8egBgwvFDvakYshE"}
+      apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
       style={{height: "100%", width: "100%"}}
       zoom={15}
       center={{lat: hgLat, lng: hgLng}}
